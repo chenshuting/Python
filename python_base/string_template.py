bdf0bbe614b5099dd3e89545d5febaa61125dd04
Shuting Chen <cicy0125@gmail.com>
2013-08-13 16:16:33 +0800

    a
    aa

--- python_base/string_template.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python 
-from string import Template
-
-s = Template('There are ${howmany} ${lang} Quotation Symblos');
-print s.substitute(lang='Python', howmany=3);
-print s.safe_substitute(lang='Python');
